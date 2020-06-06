10ad5ebb8820760f9b4bdb07d29f3b4e5d199e9d
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-06-06 13:12:16 -0300

    config nunjucks

--- src/server.js
+++ src/server.js
@@ -4,20 +4,29 @@ const server = express()
 // configurar pasta publica
 server.use(express.static("public"))
 
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true,
+})
+
+
 // configurar caminhos da minha aplicação
 // página inicial
 // req: Requisição
 // res: Resposta
 server.get("/", (req, res) => {    // rota para home
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 })
 
 server.get("/create-point", (req, res) => { // rota pra create-point
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
 server.get("/search-results", (req, res) => { // rota para results
-    res.sendFile(__dirname + "/views/search-results.html") 
+    return res.render("search-results.html") 
 })
 
 // ligar o servidor
