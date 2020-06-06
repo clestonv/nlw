929e685c3d960074f874c50f6c7c56caebf5fc47
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-06-06 12:52:21 -0300

    configurado rotas do front

--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
 const express = require("express")
 const server = express() 
 
+// configurar pasta publica
+server.use(express.static("public"))
 
 // configurar caminhos da minha aplicação
 // página inicial
 // req: Requisição
 // res: Resposta
-server.get("/", (req, res) => {
-    res.send("Cheguei aqui")
+server.get("/", (req, res) => {    // rota para home
+    res.sendFile(__dirname + "/views/index.html")
 })
 
+server.get("/create-point", (req, res) => { // rota pra create-point
+    res.sendFile(__dirname + "/views/create-point.html")
+})
 
+server.get("/search-results", (req, res) => { // rota para results
+    res.sendFile(__dirname + "/views/search-results.html") 
+})
 
 // ligar o servidor
 server.listen(3000)
